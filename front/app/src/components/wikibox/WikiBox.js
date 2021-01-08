4a0f392ee89de2a1f824df1e65c43a00baf48d86
crunchypi <crunchypi@protonmail.com>
2021-01-08 22:56:59 +0100

    hotfix: change var name

--- front/app/src/components/wikibox/WikiBox.js
+++ front/app/src/components/wikibox/WikiBox.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function WikiBox(props) {
-    let htmlString = props.signal ? signal(): "O:"
+    let htmlString = props.html ? props.html: "O:"
     return (
         <div dangerouslySetInnerHTML={{__html: htmlString}}/>
     )
