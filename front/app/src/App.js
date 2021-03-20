fd6b312c253bfe38b16e9aad0c027bb1fe596959
Malcolm <malc_isa@protonmail.com>
2021-03-20 14:11:51 +0100

    design tweaks

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Graph from "./components/graph/Graph";
 import WikiBox from "./components/wikibox/WikiBox";
-
 import SearchBar from "./components/searchbar/SearchBar.jsx";
-
 import CallbackManager from "./callbackmanager/callbackmanager.js";
-
 import NavBar from "./components/navbar/NavBar";
 
 export default class App extends Component {
   state = { callbackManager: new CallbackManager() };
 
   //import Spinner from "./components/spinner/Spinner";
-//import ConfigBox from "./components/cfgbox/ConfigBox.js";
-  //
+  //import ConfigBox from "./components/cfgbox/ConfigBox.js";
+  
   render() {
     return (
       <div className="App">
--- front/app/src/components/navbar/NavBar.js
+++ front/app/src/components/navbar/NavBar.js
@@ -2,26 +2,21 @@ import React, { Component } from "react";
 import "./NavBar.css";
 import ConfigBox from "../cfgbox/ConfigBox.js";
 
-
 export default class NavBar extends Component {
-
-    //include slider component in show hide / logic here
 
 render() {
     return (
         <div className="topnav background-2">
-        <div className="title">WikiNodes</div>
-        <a href="./app">The team</a>
-        <a href="./app">User guide</a>
-        <button className="layout-btn">Change layout</button>
-        <div className="dropdown">
+          <div className="title">WikiNodes</div>
+          <a href="./app">The team</a>
+          <a href="./app">User guide</a>
+          <div className="dropdown">
           <button className="dropbtn">
             Customize
             <i className="fa fa-caret-down"></i>
           </button>
           <div className="dropdown-content">
-            <ConfigBox />
-           
+          <ConfigBox /> 
           </div>
         </div>
       </div>
