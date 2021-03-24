bd1fc7e51924a39cac46329e3855504e0e719468
Malcolm <malc_isa@protonmail.com>
2021-03-24 11:30:19 +0100

    small css updates

--- front/app/src/components/navbar/NavBar.js
+++ front/app/src/components/navbar/NavBar.js
@@ -3,24 +3,20 @@ import "./NavBar.css";
 import ConfigBox from "../cfgbox/ConfigBox.js";
 
 export default class NavBar extends Component {
-
-render() {
+  render() {
     return (
-        <div className="topnav background-2">
-          <div className="title">WikiNodes</div>
-          <a href="./app">The team</a>
-          <a href="./app">User guide</a>
-          <div className="dropdown">
+      <div className="topnav background-2">
+        <div className="title">WikiNodes (prototype)</div>
+        <div className="dropdown">
           <button className="dropbtn">
             Customize
             <i className="fa fa-caret-down"></i>
           </button>
           <div className="dropdown-content">
-          <ConfigBox /> 
+            <ConfigBox />
           </div>
         </div>
       </div>
     );
   }
-
 }
