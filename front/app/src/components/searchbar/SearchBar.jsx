d248772813fb3dd2ac1a5eeb9fa315db55636f46
crunchypi <crunchypi@protonmail.com>
2021-03-28 18:02:18 +0200

    integrated searchbar status feedback

--- front/app/src/components/searchbar/SearchBar.jsx
+++ front/app/src/components/searchbar/SearchBar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Spinner from "../spinner/Spinner";
+import * as api from '../../api/api.js'
+
 import "../spinner/Spinner.css";
 import "./SearchBar.css";
 
@@ -7,16 +9,35 @@ export default class SearchBar extends Component {
   state = { currentText: "", 
             resultReturned: true};
 
+  // # Called when text input is completed. Uses input to
+  // # fetch content from the backend and send it along to
+  // # everyone that is registered in the callbackmanager
+  // # for this func. Not super clean impl but better than
+  // # the previous alternative.
   done = () => {
+    // # Get observers.
     let { callbackManager } = this.props;
     let callbacks = callbackManager.callbackFuncs(
       // # Curernt cls and func.
       "SearchBar",
       "done"
     );
-    // # Send to all observers.
-    callbacks.forEach((f) => f(this.state.currentText));
-  };
+    // # Try request and pass to observers.
+    api.searchArticlesByContent(this.state.currentText, 1)
+      .then(resp => {
+        if (resp.length == 0) {
+            this.setState({resultReturned: false})
+            return
+        }
+        this.setState({resultReturned: true})
+        // # Send to all observers.
+        callbacks.forEach((f) => f(resp));
+
+      })
+      .catch(_ => {
+        this.setState({resultReturned: false})
+      })
+};
    
   textInput = () => {
     return (
