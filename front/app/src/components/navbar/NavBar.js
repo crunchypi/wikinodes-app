1d7f9daff19cc999c5653ccebbacc7489c180942
Malcolm <malc_isa@protonmail.com>
2021-03-29 20:54:40 +0200

    indents

--- front/app/src/components/navbar/NavBar.js
+++ front/app/src/components/navbar/NavBar.js
@@ -3,20 +3,20 @@ import "./NavBar.css";
 import ConfigBox from "../cfgbox/ConfigBox.js";
 
 export default class NavBar extends Component {
-  render() {
-    return (
-      <div className="topnav background-2">
-        <div className="title">WikiNodes (prototype)</div>
-        <div className="dropdown">
-          <button className="dropbtn">
-            Customize
-            <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <ConfigBox />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="topnav background-2">
+                <div className="title">WikiNodes (prototype)</div>
+                <div className="dropdown">
+                    <button className="dropbtn">
+                        Customize
+                        <i className="fa fa-caret-down"></i>
+                    </button>
+                    <div className="dropdown-content">
+                        <ConfigBox />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
